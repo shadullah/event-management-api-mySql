8b9b369eaf034da4d25b430d3e762d5e7746e5d5
hexaalif <shadullahsad9@gmail.com>
2024-09-02 15:17:53 +0600

    MYSQL  cloud added

--- src/db/index.js
+++ src/db/index.js
@@ -3,10 +3,10 @@ import mysql2 from "mysql2/promise";
 const connectDb = async () => {
   try {
     const connection = await mysql2.createConnection({
-      host: "127.0.0.1",
-      user: "root",
-      password: "Mysql@7675",
-      database: "event_management_db_with_node",
+      host: process.env.HOST,
+      user: process.env.USER,
+      password: process.env.PASSWORD,
+      database: process.env.DATABASE_NAME,
     });
     console.log(`\n mysql connected !! db Host`, connection.config.host);
     return connection;
